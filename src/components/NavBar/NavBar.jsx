201691076fe0dd7a12d66f85c052210698080a90
Martin Ferraro <arqmartinferraro@gmail.com>
2022-04-13 12:20:02 -0300

    inicio Navbar app

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './NavBar.css';
+
+function NavBar() {
+    return <>
+        <NavBar />
+        <h6>Prueba de encabezado</h6>
+    </>;
+}
+
+export default NavBar
