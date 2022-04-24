22818dd2960faff4bf063d826fa4a533197af98d
Martin Ferraro <arqmartinferraro@gmail.com>
2022-04-24 15:14:16 -0300

    Cambios varios

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -11,15 +11,18 @@ function ItemCount({ stock, initial,  onAdd }) {
             console.log(count + ' añadidas al carrito')
             console.log((stockVar - count) + ' en stock')
             setStock(stockVar - count)
+            setCount(1)
         } else if (count > stockVar > 0) {
             console.log(stockVar + ' añadidas al carrito')
             console.log('Stock agotado')
             setStock(0)
+            setCount(0)
             setDisabled(true)
         } else {
             console.log(count + ' añadidas al carrito')
             console.log('Stock agotado')
             setStock(0)
+            setCount(0)
             setDisabled(true)
         }
     }
@@ -39,9 +42,9 @@ function ItemCount({ stock, initial,  onAdd }) {
     return <>
         <div className='d-flex flex-column col-2'>
             <div className='d-flex flex-row align-items-center justify-content-between mb-2'>
-                <button className='btnAddSub' onClick={() => restaCant()}><i class='bi bi-dash-circle-fill'/></button>
+                <button className='btnAddSub' onClick={() => restaCant()} disabled={isDisabled}><i class='bi bi-dash-circle-fill'/></button>
                 <p className='m-0 mx-3'>{count}</p>
-                <button className='btnAddSub' onClick={() => sumaCant()}><i class='bi bi-plus-circle-fill'/></button>
+                <button className='btnAddSub' onClick={() => sumaCant()} disabled={isDisabled}><i class='bi bi-plus-circle-fill'/></button>
             </div>
             <button className='btnAddSub p-2' onClick={() => onAdd()} disabled={isDisabled}>Agregar al Carrito</button>
         </div>
