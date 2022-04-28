78e70abb1a74e05da2e3a90f7143dcc7843d2871
Martin Ferraro <arqmartinferraro@gmail.com>
2022-04-28 20:03:04 -0300

    Comienzo routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,18 @@ import './App.css'
 import React from 'react'
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Catalogo from './pages/Catalogo'
+import Detalle from './pages/Detalle'
 
 function App() {
   return <>
+    <Routes>
+      <Route path='/home' element={<Home/>} />
+      <Route path='/catalogo' element={<Catalogo/>} />
+      <Route path='/detalle' element={<Detalle/>} />
+    </Routes>
     <NavBar />
     <ItemListContainer txt = 'Prueba contador'/>
   </>
--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
